d113251908c7b49dc2a95ef5e62c24f9e3c2d75d
MarioMercurio <mercuriom@xavier.edu>
2026-06-07 19:44:45 -0400

    Add files via upload

--- gun_pages/gun_home.py
+++ gun_pages/gun_home.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 import streamlit as st
 
-from gun_components.gun_layout import gun_nav_button
 from gun_utils.gun_constants import GUN_LOGO_PATH, GUN_QUOTE
 
 
@@ -14,10 +13,17 @@ def gun_render_home():
     else:
         st.markdown("<h1>GUN CLUB</h1>", unsafe_allow_html=True)
 
-    gun_nav_button("LOG WORKOUT", "log_workout")
-    gun_nav_button("STATS", "stats")
-    gun_nav_button("RECORD BOOK", "record_book")
-    gun_nav_button("WEEK-BY-WEEK", "week_by_week")
-    gun_nav_button("WORKOUT HISTORY", "workout_history")
+    st.markdown(
+        """
+        <div class="gun-home-buttons">
+            <a class="gun-home-button" href="?gun_page=log_workout">LOG WORKOUT</a>
+            <a class="gun-home-button" href="?gun_page=stats">STATS</a>
+            <a class="gun-home-button" href="?gun_page=record_book">RECORD BOOK</a>
+            <a class="gun-home-button" href="?gun_page=week_by_week">WEEK-BY-WEEK</a>
+            <a class="gun-home-button" href="?gun_page=workout_history">WORKOUT HISTORY</a>
+        </div>
+        """,
+        unsafe_allow_html=True,
+    )
 
     st.markdown(f'<div class="gun-quote">{GUN_QUOTE}</div>', unsafe_allow_html=True)
